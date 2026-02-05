3914e5608be8fe60e71810b6f4b93acc6a6e0f08
Gio Ciudadano <gcciudadano@gmail.com>
2026-02-06 01:17:13 +0800

    Add async fetch for getting data by tag

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,25 +2,25 @@ import React, { useEffect, useState } from 'react';
 
 import GameCards from '../components/Home/GameCards/GameCards';
 import GameCard from '../components/Home/GameCard/GameCard.types';
-import getGameCards from '../utils/getGameCards';
+import { getGameCards, getGameCardsByTag } from '../utils/getGameCards';
 
 import TagFilter from '../components/Home/TagFilter/TagFilter';
 import { TAGS, TagName } from '../components/Home/Tag.types';
 
 const Home: React.FC = () => {
-  const [allGameCards, setAllGameCards] = useState<GameCard[]>([]);
   const [gameCards, setGameCards] = useState<GameCard[]>([]);
 
   useEffect(() => {
-    getGameCards().then(data => {
-      setAllGameCards(data);
+    getGameCards().then((data: GameCard[]) => {
       setGameCards(data);
     });
   }, []);
 
   const onTagSelect = (tagName: TagName) => {
-    const filteredCards = allGameCards.filter(card => card.tags.includes(tagName));
-    setGameCards(filteredCards);
+    setGameCards([]);
+    getGameCardsByTag(tagName).then((data: GameCard[]) => {
+      setGameCards(data);
+    });
   };
 
   return (
--- src/utils/getGameCards.ts
+++ src/utils/getGameCards.ts
@@ -1,6 +1,7 @@
 import gameCardsData from '../data/gameCards.json';
 import GameCard from '../components/Home/GameCard/GameCard.types';
 import { PROVIDERS, ProviderName } from '../components/Home/Provider.types';
+import Tag, { TagName } from '../components/Home/Tag.types';
 
 // Helper to check if the passed value is a valid Provider
 function isValidProvider(value: any): value is ProviderName {
@@ -23,13 +24,23 @@ function mapJsonToGameCard(json: any): GameCard {
   };
 }
 
-// Simulates an asynchronous fetch of game cards data
-function getGameCards(): Promise<GameCard[]> {
+const LOADING_DELAY_MS = 1000;
+
+// Simulates an asynchronous fetch of all game cards data
+export function getGameCards(): Promise<GameCard[]> {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve((gameCardsData as any[]).map(mapJsonToGameCard));
-    }, 3000);
+    }, LOADING_DELAY_MS);
   });
 }
 
-export default getGameCards;
+// Simulates an asynchronous fetch of game cards filtered by a specific tag
+export function getGameCardsByTag(tagName: TagName): Promise<GameCard[]> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const filteredCards = (gameCardsData as any[]).filter(card => card.tags.includes(tagName));
+      resolve(filteredCards.map(mapJsonToGameCard));
+    }, LOADING_DELAY_MS);
+  });
+}
