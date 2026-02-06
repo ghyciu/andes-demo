1714858334baaa5226a18cfe3827382ed224e7a9
Gio Ciudadano <gcciudadano@gmail.com>
2026-02-06 13:54:00 +0800

    Add provider selection partial functionality

--- src/components/Home/ProviderFilter/ProviderFilter.tsx
+++ src/components/Home/ProviderFilter/ProviderFilter.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { PROVIDERS, Provider } from '../Provider.types';
+import { Provider, ProviderName } from '../Provider.types';
 import ProviderFilterItem from '../ProviderFilterItem/ProviderFilterItem';
 import GameCard from '../GameCard/GameCard.types';
 import styles from './ProviderFilter.module.css';
@@ -11,13 +11,25 @@ interface ProviderFilterProps {
 }
 
 const ProviderFilter: React.FC<ProviderFilterProps> = ({ providers, gameCards }) => {
+  const [selectedProvider, setSelectedProvider] = useState<ProviderName | null>(null);
+
   const activeProviderNames = Array.from(new Set(gameCards.map(card => card.provider)));
   const filteredProviders = providers.filter(provider => activeProviderNames.includes(provider.name));
+
+  const onSelect = (providerName: ProviderName) => {
+    setSelectedProvider(providerName);
+  };
 
   return (
     <div className={styles.providerFilter}>
       {filteredProviders.map((provider, index) => (
-        <ProviderFilterItem key={`provider-${index}`} provider={provider} count={gameCards.filter(card => card.provider === provider.name).length} />
+        <ProviderFilterItem
+          key={`provider-${index}`}
+          provider={provider}
+          count={gameCards.filter(card => card.provider === provider.name).length}
+          isSelected={selectedProvider === provider.name}
+          onSelect={onSelect}
+        />
       ))}
     </div>
   );
--- src/components/Home/ProviderFilterItem/ProviderFilterItem.tsx
+++ src/components/Home/ProviderFilterItem/ProviderFilterItem.tsx
@@ -1,14 +1,20 @@
-import { Provider } from '../Provider.types';
+import { Provider, ProviderName } from '../Provider.types';
 import styles from './ProviderFilterItem.module.css';
 
 interface ProviderFilterItemProps {
   provider: Provider;
   count: number;
+  isSelected: boolean;
+  onSelect: (providerName: ProviderName) => void;
 }
 
-const ProviderFilterItem: React.FC<ProviderFilterItemProps> = ({ provider, count }) => {
+const ProviderFilterItem: React.FC<ProviderFilterItemProps> = ({ provider, count, isSelected, onSelect }) => {
+  const handleClick = () => {
+    onSelect(provider.name);
+  };
+
   return (
-    <div className={styles.providerItem}>
+    <div className={`${styles.providerItem} ${isSelected ? styles.selected : ''}`} onClick={handleClick}>
       <img src={provider.imageURL} alt={provider.name} className={styles.providerImage} />
       <span className={styles.providerCount}>({count})</span>
     </div>
